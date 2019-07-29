4b4c5118d1095d6f290bbab285849d055eb2a968
naru-taka <aloha.46.naru@me.com>
2019-07-29 17:14:57 +0900

    アホ、消したよ

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,7 +1,7 @@
 class ArticlesController < ApplicationController
   before_action :set_article, only: [:show, :edit, :update, :destroy]
 
-#  　　あほ
+  
   def index
     @articles = Article.all
   end
